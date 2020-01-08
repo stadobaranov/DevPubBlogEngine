134f4445023d8f0b31271a0605ca3a4f799a29a1
stadobaranov <unnamedx.xb@gmail.com>
2020-01-08 21:13:34 +0300

    Добавление обработки действия "GET /"

--- src/main/kotlin/devpub/blogengine/controller/DefaultController.kt
+++ src/main/kotlin/devpub/blogengine/controller/DefaultController.kt
@@ -1,6 +1,10 @@
 package devpub.blogengine.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-open class DefaultController
+open class DefaultController {
+    @GetMapping
+    open fun index() = "index.html"
+}
