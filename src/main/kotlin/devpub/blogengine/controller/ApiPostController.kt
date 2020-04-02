a419950c0cd755886fd6b81d24448c2818264a1e
stadobaranov <unnamedx.xb@gmail.com>
2020-04-03 01:48:08 +0300

    Добавление обработки действия "POST /api/post"

--- src/main/kotlin/devpub/blogengine/controller/ApiPostController.kt
+++ src/main/kotlin/devpub/blogengine/controller/ApiPostController.kt
@@ -9,11 +9,16 @@ import devpub.blogengine.model.PostPageByTagRequest
 import devpub.blogengine.model.PostPageForCurrentUserRequest
 import devpub.blogengine.model.PostPageRequest
 import devpub.blogengine.model.PostPageResponse
+import devpub.blogengine.model.SavePostRequest
 import devpub.blogengine.service.PostPageService
 import devpub.blogengine.service.PostService
+import devpub.blogengine.service.ValidationErrorsResponseMaker
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
@@ -22,7 +27,8 @@ import javax.validation.Valid
 @RequestMapping("api/post")
 open class ApiPostController @Autowired constructor(
     private val postPageService: PostPageService,
-    private val postService: PostService
+    private val postService: PostService,
+    private val validationErrorsResponseMaker: ValidationErrorsResponseMaker
 ) {
     @GetMapping
     open fun getPage(request: PostPageRequest): PostPageResponse {
@@ -57,5 +63,14 @@ open class ApiPostController @Autowired constructor(
     @GetMapping("{id}")
     open fun getDetails(@PathVariable("id") id: Int): DetailedPostResponse {
         return postService.getDetails(id)
+    }
+
+    @PostMapping
+    open fun create(@Valid @RequestBody request: SavePostRequest, bResult: BindingResult): Any {
+        if(bResult.hasFieldErrors()) {
+            return validationErrorsResponseMaker.make(SavePostRequest::class, bResult.fieldErrors)
+        }
+
+        return postService.create(request)
     }
 }
