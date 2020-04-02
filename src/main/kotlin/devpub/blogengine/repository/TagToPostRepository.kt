a44f2cbd9c580431fa569f0aa4e8eb43e7127fae
stadobaranov <unnamedx.xb@gmail.com>
2020-04-02 21:18:32 +0300

    Добавление репозитория TagToPostRepository

---
+++ src/main/kotlin/devpub/blogengine/repository/TagToPostRepository.kt
@@ -0,0 +1,7 @@
+package devpub.blogengine.repository
+
+import devpub.blogengine.model.entity.TagToPost
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TagToPostRepository: BaseRepository<TagToPost>
