008052d0c68f3dd94779bbaf0a26d8f54825937f
stadobaranov <unnamedx.xb@gmail.com>
2020-04-02 14:02:08 +0300

    Добавление репозитория PostRepository

---
+++ src/main/kotlin/devpub/blogengine/repository/PostRepository.kt
@@ -0,0 +1,7 @@
+package devpub.blogengine.repository
+
+import devpub.blogengine.model.entity.Post
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PostRepository: BaseRepository<Post>
