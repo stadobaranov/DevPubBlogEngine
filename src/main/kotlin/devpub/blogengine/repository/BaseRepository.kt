e078dbfc6069c030908a01004f60cf8a94428a7c
stadobaranov <unnamedx.xb@gmail.com>
2020-04-02 11:43:26 +0300

    Добавление базового репозитория BaseRepository
    BaseRepository расширяет интерфейс JpaRepository, добавляя методы
    с писсимистической блокировкой и удаления.

---
+++ src/main/kotlin/devpub/blogengine/repository/BaseRepository.kt
@@ -0,0 +1,61 @@
+package devpub.blogengine.repository
+
+import devpub.blogengine.model.entity.Persistent
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.NoRepositoryBean
+import org.springframework.data.repository.query.Param
+import javax.persistence.LockModeType
+
+@NoRepositoryBean
+interface BaseRepository<T: Persistent>: JpaRepository<T, Int> {
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Query("""
+        select e.id 
+        from #{#entityName} e
+        where e.id = :id
+    """)
+    fun lockForShare(@Param("id") id: Int): Int?
+
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Query("""
+        select e 
+        from #{#entityName} e
+        where e.id = :id
+    """)
+    fun findAndLockForShare(@Param("id") id: Int): T?
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+        select e.id 
+        from #{#entityName} e
+        where e.id = :id
+    """)
+    fun lockForUpdate(@Param("id") id: Int): Int?
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+        select e 
+        from #{#entityName} e
+        where e.id = :id
+    """)
+    fun findAndLockForUpdate(@Param("id") id: Int): T?
+
+    @Modifying
+    @Query("""
+        delete 
+        from #{#entityName} e
+        where e.id = :id
+    """)
+    fun deleteOne(@Param("id") id: Int): Int
+
+    @Modifying
+    @Query("""
+        delete 
+        from #{#entityName} e
+        where e.id in :ids
+    """)
+    fun deleteAll(@Param("ids") ids: Set<Int>): Int
+}
