68e80487f5e6b9aaa2729149a722b17768bf0365
stadobaranov <unnamedx.xb@gmail.com>
2020-04-03 01:51:15 +0300

    Добавление обработки действия "GET /api/calendar"

--- src/main/kotlin/devpub/blogengine/controller/ApiGeneralController.kt
+++ src/main/kotlin/devpub/blogengine/controller/ApiGeneralController.kt
@@ -1,6 +1,9 @@
 package devpub.blogengine.controller
 
 import devpub.blogengine.model.InitResponse
+import devpub.blogengine.model.PostCountToDatesRequest
+import devpub.blogengine.model.PostCountToDatesResponse
+import devpub.blogengine.service.PostService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,10 +18,16 @@ open class ApiGeneralController @Autowired constructor(
     @Value("\${blog-engine.contact-phone}") private val contactPhone: String,
     @Value("\${blog-engine.contact-email}") private val contactEmail: String,
     @Value("\${blog-engine.copyright}") private val copyright: String,
-    @Value("\${blog-engine.copyright-from}") private val copyrightFrom: String
+    @Value("\${blog-engine.copyright-from}") private val copyrightFrom: String,
+    private val postService: PostService
 ) {
     @GetMapping("init")
     open fun init(): InitResponse {
         return InitResponse(frontendTitle, frontendSubTitle, contactPhone, contactEmail, copyright, copyrightFrom)
+    }
+
+    @GetMapping("calendar")
+    open fun getPostCountByDate(request: PostCountToDatesRequest): PostCountToDatesResponse {
+        return postService.getCountToDates(request)
     }
 }
