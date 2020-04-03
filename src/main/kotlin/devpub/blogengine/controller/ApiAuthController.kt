1002aa656b4183e83c19e35ac0250638f8f60928
stadobaranov <unnamedx.xb@gmail.com>
2020-04-03 16:59:44 +0300

    Добавление обработки действия "POST /api/auth/login"

--- src/main/kotlin/devpub/blogengine/controller/ApiAuthController.kt
+++ src/main/kotlin/devpub/blogengine/controller/ApiAuthController.kt
@@ -1,11 +1,15 @@
 package devpub.blogengine.controller
 
 import devpub.blogengine.model.AuthorizedUserResponse
+import devpub.blogengine.model.LoginUserRequest
 import devpub.blogengine.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("api/auth")
@@ -15,5 +19,10 @@ open class ApiAuthController @Autowired constructor(
     @GetMapping("check")
     open fun checkUserLoggedIn(): AuthorizedUserResponse {
         return userService.checkLoggedIn()
+    }
+
+    @PostMapping("login")
+    open fun loginUser(@Valid @RequestBody request: LoginUserRequest): AuthorizedUserResponse {
+        return userService.login(request)
     }
 }
