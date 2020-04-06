7a04699caebe6954d36609eb7f3ad6a1864a47db
stadobaranov <unnamedx.xb@gmail.com>
2020-04-07 00:57:45 +0300

    Добавление репозитория PostVoteRepository

---
+++ src/main/kotlin/devpub/blogengine/repository/PostVoteRepository.kt
@@ -0,0 +1,7 @@
+package devpub.blogengine.repository
+
+import devpub.blogengine.model.entity.PostVote
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PostVoteRepository: BaseRepository<PostVote>
