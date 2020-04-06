e18625faf65bc670f1b8a00de952d51d0b129e02
stadobaranov <unnamedx.xb@gmail.com>
2020-04-06 21:25:40 +0300

    Переадресация SPA-путей на "/"

--- src/main/kotlin/devpub/blogengine/ApplicationConfiguration.kt
+++ src/main/kotlin/devpub/blogengine/ApplicationConfiguration.kt
@@ -2,8 +2,14 @@ package devpub.blogengine
 
 import devpub.blogengine.service.UploadStorage
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.server.ErrorPage
+import org.springframework.boot.web.server.WebServerFactoryCustomizer
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -13,5 +19,16 @@ open class ApplicationConfiguration(
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("${UploadStorage.BASE_URL}**")
                 .addResourceLocations("file:$uploadDir")
+    }
+
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/notFound").setViewName("forward:/")
+    }
+
+    @Bean
+    open fun servletContainerCustomizer(): WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+        return WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+            factory -> factory.addErrorPages(ErrorPage(HttpStatus.NOT_FOUND, "/notFound"))
+        }
     }
 }
