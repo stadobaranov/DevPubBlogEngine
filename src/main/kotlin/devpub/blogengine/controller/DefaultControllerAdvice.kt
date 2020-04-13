adf8bb450dcf1801be07e1394e203bd44e82d8dd
stadobaranov <unnamedx.xb@gmail.com>
2020-04-13 18:14:27 +0300

    Рефакторинг DefaultControllerAdvice

--- src/main/kotlin/devpub/blogengine/controller/DefaultControllerAdvice.kt
+++ src/main/kotlin/devpub/blogengine/controller/DefaultControllerAdvice.kt
@@ -8,24 +8,15 @@ import devpub.blogengine.service.exception.ModelIntegrityException
 import devpub.blogengine.service.exception.ModelNotFoundException
 import devpub.blogengine.service.exception.UnauthorizedException
 import org.slf4j.LoggerFactory
-import org.springframework.beans.TypeMismatchException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.propertyeditors.StringTrimmerEditor
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.http.converter.HttpMessageNotReadableException
-import org.springframework.validation.BindException
-import org.springframework.web.HttpMediaTypeNotAcceptableException
-import org.springframework.web.HttpMediaTypeNotSupportedException
-import org.springframework.web.HttpRequestMethodNotSupportedException
-import org.springframework.web.bind.MethodArgumentNotValidException
-import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.WebDataBinder
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.InitBinder
 import org.springframework.web.multipart.MaxUploadSizeExceededException
-import org.springframework.web.multipart.support.MissingServletRequestPartException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -40,27 +31,24 @@ open class DefaultControllerAdvice @Autowired constructor(
         dataBinder.registerCustomEditor(String::class.java, StringTrimmerEditor(false))
     }
 
-    @ExceptionHandler(Exception::class)
-    open fun handleException(exception: Exception, request: HttpServletRequest, response: HttpServletResponse): Any {
-        return when(exception) {
-            is UnauthorizedException -> buildResponseForUnauthorized()
-            is ModelIntegrityException -> buildResponseForBadRequest()
-            is ModelNotFoundException -> buildResponseForNotFound()
-            is GlobalSettingValueConversionException -> buildResponseForBadRequest()
-            is BindException -> buildResponseForBadRequest()
-            is MethodArgumentNotValidException -> buildResponseForBadRequest()
-            is HttpMessageNotReadableException -> buildResponseForBadRequest()
-            is MissingServletRequestParameterException -> buildResponseForBadRequest()
-            is MissingServletRequestPartException -> buildResponseForBadRequest()
-            is TypeMismatchException -> buildResponseForBadRequest()
-            is HttpRequestMethodNotSupportedException -> buildResponseForMethodNotAllowed()
-            is HttpMediaTypeNotAcceptableException -> buildResponseForNotAcceptable()
-            is HttpMediaTypeNotSupportedException -> buildResponseForUnsupportedMediaType()
-            else -> {
-                logger.error("Поймано необработанное исключение", exception)
-                buildEmptyResponse(HttpStatus.INTERNAL_SERVER_ERROR)
-            }
-        }
+    @ExceptionHandler(UnauthorizedException::class)
+    open fun handleUnauthorizedException(): Any {
+        return buildEmptyResponse(HttpStatus.UNAUTHORIZED)
+    }
+
+    @ExceptionHandler(ModelIntegrityException::class)
+    open fun handleModelIntegrityException(): Any {
+        return buildResponseForBadRequest()
+    }
+
+    @ExceptionHandler(ModelNotFoundException::class)
+    open fun handleModelNotFoundException(): Any {
+        return buildEmptyResponse(HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(GlobalSettingValueConversionException::class)
+    open fun handleGlobalSettingValueConversionException(): Any {
+        return buildResponseForBadRequest()
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException::class)
@@ -72,29 +60,8 @@ open class DefaultControllerAdvice @Autowired constructor(
         return maxUploadSizeExceededExceptionHandlingService.handle(exception, request, response) ?: buildResponseForBadRequest()
     }
 
-    private fun buildResponseForUnauthorized(): ResponseEntity<Any> {
-        return buildEmptyResponse(HttpStatus.UNAUTHORIZED)
-    }
-
     private fun buildResponseForBadRequest(): ResponseEntity<Any> {
-        return ResponseEntity.badRequest()
-                             .body(MessageResponse(ApplicationMessages.BAD_REQUEST))
-    }
-
-    private fun buildResponseForNotFound(): ResponseEntity<Any> {
-        return buildEmptyResponse(HttpStatus.NOT_FOUND)
-    }
-
-    private fun buildResponseForMethodNotAllowed(): ResponseEntity<Any> {
-        return buildEmptyResponse(HttpStatus.METHOD_NOT_ALLOWED)
-    }
-
-    private fun buildResponseForUnsupportedMediaType(): ResponseEntity<Any> {
-        return buildEmptyResponse(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-    }
-
-    private fun buildResponseForNotAcceptable(): ResponseEntity<Any> {
-        return buildEmptyResponse(HttpStatus.NOT_ACCEPTABLE)
+        return ResponseEntity.badRequest().body(MessageResponse(ApplicationMessages.BAD_REQUEST))
     }
 
     private fun buildEmptyResponse(status: HttpStatus): ResponseEntity<Any> {
