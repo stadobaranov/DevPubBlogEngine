56c2a8af8cc5d287435a31df9d909b4bf71eeb87
stadobaranov <unnamedx.xb@gmail.com>
2020-04-14 15:23:51 +0300

    Удаление сервиса GlobalSettingDefinitionProvider

--- src/main/kotlin/devpub/blogengine/model/GlobalSettingDefinition.kt
+++
@@ -1,6 +0,0 @@
-package devpub.blogengine.model
-
-class GlobalSettingDefinition {
-    lateinit var defaultName: String
-    lateinit var defaultValue: String
-}
--- src/main/kotlin/devpub/blogengine/service/GlobalSettingDefinitionProvider.kt
+++
@@ -1,8 +0,0 @@
-package devpub.blogengine.service
-
-import devpub.blogengine.model.GlobalSettingDefinition
-import devpub.blogengine.model.entity.GlobalSetting
-
-interface GlobalSettingDefinitionProvider {
-    fun get(code: GlobalSetting.Code): GlobalSettingDefinition
-}
