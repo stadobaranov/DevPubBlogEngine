1a7ba66e3cdb1d2ba021f26aa5aef14a645259b1
stadobaranov <unnamedx.xb@gmail.com>
2020-04-17 18:37:00 +0300

    Уменьшение размера шрифта генерируемой капчи

--- src/main/kotlin/devpub/blogengine/service/CaptchaImageCreatorImpl.kt
+++ src/main/kotlin/devpub/blogengine/service/CaptchaImageCreatorImpl.kt
@@ -1,19 +1,24 @@
 package devpub.blogengine.service
 
 import nl.captcha.Captcha
+import nl.captcha.text.producer.TextProducer
+import nl.captcha.text.renderer.DefaultWordRenderer
 import org.springframework.stereotype.Service
+import java.awt.Color
+import java.awt.Font
 import java.awt.image.BufferedImage
+import java.util.Collections
 
 private const val IMAGE_WIDTH = 103
 private const val IMAGE_HEIGHT = 56
+private val colors = Collections.singletonList(Color.BLACK)
+private val fonts = Collections.singletonList(Font("Arial", Font.BOLD, 32))
 
 @Service
 open class CaptchaImageCreatorImpl: CaptchaImageCreator {
     override fun create(code: String): BufferedImage {
-        val textProducer = { code }
-
         return Captcha.Builder(IMAGE_WIDTH, IMAGE_HEIGHT)
-                .addText(textProducer)
+                .addText(TextProducer { code }, DefaultWordRenderer(colors, fonts))
                 .addBorder()
                 .build()
                 .image
